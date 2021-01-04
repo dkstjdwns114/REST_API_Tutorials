4c2a0f6ce5df62ba02732336b36ccedeb71cf785
dkstjdwns114 <sungjoon256@gmail.com>
2021-01-04 19:34:25 +0900

    Setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+// set up express app
+const app = express();
+
+app.get("/api", function (req, res) {
+  console.log("GET request");
+  res.send({ name: "Banana" });
+});
+
+// listen for requests
+app.listen(process.env.port || 4000, function () {
+  console.log("now listening for requests");
+});
