6f144f033467ac15d2b719a7e9201eea006c087f
dkstjdwns114 <sungjoon256@gmail.com>
2021-01-04 20:44:06 +0900

    Error Handling

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.use(bodyParser.json());
 // initialize routes
 app.use("/api", require("./routes/api"));
 
+// error handling middleware
+app.use(function (error, req, res, next) {
+  // console.log(error);
+  res.status(422).send({ error: error._message });
+});
+
 // listen for requests
 app.listen(process.env.port || 4000, function () {
   console.log("now listening for requests");
