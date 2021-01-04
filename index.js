f93cf2c25797535f68843ea0eec5594ac1f8d265
dkstjdwns114 <sungjoon256@gmail.com>
2021-01-04 22:57:33 +0900

    Creating a Front-end

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ mongoose.connect("mongodb://localhost/ninjago", {
 });
 mongoose.Promise = global.Promise;
 
+app.use(express.static("public"));
+
 app.use(bodyParser.json());
 
 // initialize routes
